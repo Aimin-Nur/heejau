0bebc91c722d5295afe9b96663eff34ae2b4560e
muhaimin nur <you@example.com>
2024-02-26 22:09:38 +0800

    add page swasta and controller for the actor

---
+++ resources/views/swasta/layouts/header.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+    <title>Heejau - Malewa</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('swasta')}}/styles/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('swasta')}}/fonts/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@500;600;700&amp;family=Roboto:wght@400;500;700&amp;display=swap" rel="stylesheet">
+    <link rel="manifest" href="_manifest.html">
+    <meta id="theme-check" name="theme-color" content="#FFFFFF">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('swasta')}}/app/icons/icon-192x192.html">
+</head>
+
+    <body class="theme-light">
+        <div id="preloader">
+            <div class="spinner-border color-highlight" role="status"></div>
+        </div>
+
+        <div id="page">
+
+        <div id="footer-bar" class="footer-bar-1 footer-bar-detached">
+            <a href="page-wallet.html" class="active-nav"><i class="bi bi-house-fill"></i><span>Home</span></a>
+            {{-- <a href="page-activity.html"> <i class="bi bi-graph-up"></i> <span>Activity</span></a> --}}
+            <a href="index-waves.html" class="circle-nav-2"><i class="bi bi-camera-fill"></i><span>Kamera</span></a>
+            <a href="page-payments.html"><i class="bi bi-receipt"></i><span>Profil</span></a>
+            {{-- <a href="#" data-bs-toggle="offcanvas" data-bs-target="#menu-sidebar"><i class="bi bi-three-dots"></i><span>More</span></a> --}}
+        </div>
---
+++ resources/views/swasta/layouts/script.blade.php
@@ -0,0 +1,3 @@
+<script src="{{asset('swasta')}}/scripts/bootstrap.min.js"></script>
+<script src="{{asset('swasta')}}/scripts/custom.js"></script>
+</body>
